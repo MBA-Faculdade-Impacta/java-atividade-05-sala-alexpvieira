861451dfb782549d02b72ca4944fb1b388180c0d
Alex Pereira Vieira <alex.p.vieira@gmail.com>
2021-03-03 00:32:43 +0000

    Criando o objeto produto

---
+++ src/main/java/br/com/impacta/models/Produto.java
@@ -0,0 +1,12 @@
+package br.com.impacta.models;
+
+public class Produto {
+  public int codigo;
+  public String descricao;
+  public double valor;
+
+  public String montarResumo() {
+    String resumo = "Código do produto é: " + this.codigo + " descrição: " + this.descricao + " valor: " + this.valor;
+    return resumo;
+  }
+}
